505b5267b7781bafb935fc717c97c632826166dd
Bhavishya <bhavishya1102@gmail.com>
2024-07-06 08:45:44 +0530

    init pages

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'tamagui'
+
+export default function Settings() {
+  return (
+    <View flex={1} alignItems="center" justifyContent="center">
+      <Text fontSize={20} color="$blue10">
+        Settings
+      </Text>
+    </View>
+  )
+}
