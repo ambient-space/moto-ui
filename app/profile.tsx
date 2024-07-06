70963ca86581471bb8311758d151a21e4aa335b7
Bhavishya <bhavishya1102@gmail.com>
2024-07-06 08:47:33 +0530

    init profile page

---
+++ app/profile.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'tamagui'
+
+export default function ProfileScreen() {
+  return (
+    <View flex={1} alignItems="center" justifyContent="center">
+      <Text fontSize={20} color="$blue10">
+        Profile
+      </Text>
+    </View>
+  )
+}
