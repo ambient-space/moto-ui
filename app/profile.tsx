cb438d0cca8808d0dfda8c106076179db30641e6
Bhavishya <bhavishya1102@gmail.com>
2024-07-07 12:48:38 +0530

    feat: Add profile screen with user information and avatar
    This commit adds a new file, profile.tsx, which contains the implementation of the ProfileScreen component. The ProfileScreen component displays the user's profile information, including their avatar, username, bio, and a button for initiating a chat. It also includes lazy loading for images to improve page load performance. This addition enhances the user experience by providing a dedicated screen for viewing and interacting with the user's profile.

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,11 +1,47 @@
-import { Text, View } from 'tamagui'
+import { SafeAreaView } from 'react-native'
+import { Avatar, Image, Text, View, YStack, Button, Paragraph, H3 } from 'tamagui'
 
 export default function ProfileScreen() {
   return (
-    <View flex={1} alignItems="center" justifyContent="center">
-      <Text fontSize={20} color="$blue10">
-        Profile
-      </Text>
+    <View>
+      <SafeAreaView>
+        <YStack>
+          <Image
+            source={{
+              uri: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
+              height: 140,
+            }}
+          />
+          <YStack gap="$2" p="$4" ai="center">
+            <Avatar
+              size="$8"
+              mt="$-10"
+              circular
+              borderColor="white"
+              borderWidth="$1"
+              backgroundColor="$blue10"
+            >
+              <Avatar.Image
+                source={{
+                  uri: 'https://randomuser.me/api/portraits/women/12.jpg',
+                  height: 140,
+                }}
+              />
+              <Avatar.Fallback />
+            </Avatar>
+            <YStack ai="center" gap="$2">
+              <H3>User name</H3>
+              <Paragraph color="$color05" mt="$-3">
+                @username
+              </Paragraph>
+              <Text>This is the user's bio</Text>
+            </YStack>
+            <Button size="$3" mt="$4" w="100%">
+              Chat
+            </Button>
+          </YStack>
+        </YStack>
+      </SafeAreaView>
     </View>
   )
 }
