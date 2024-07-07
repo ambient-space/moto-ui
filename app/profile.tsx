842985d5fc6f36678a5cf635c6a57af112c85856
Bhavishya <bhavishya1102@gmail.com>
2024-07-07 20:16:21 +0530

    refactor: Update ProfileScreen component with user data and redirect
    The ProfileScreen component in profile.tsx has been updated to include user data and a redirect if the user is not logged in. This refactor improves the user experience by displaying the user's cover image, profile picture, full name, username, and bio. If the user is not logged in, they will be redirected to the home page.

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,14 +1,22 @@
+import useAuthStore from '@/state/authStore'
+import { Redirect } from 'expo-router'
 import { SafeAreaView } from 'react-native'
 import { Avatar, Image, Text, View, YStack, Button, Paragraph, H3 } from 'tamagui'
 
 export default function ProfileScreen() {
+  const user = useAuthStore((state) => state.user)
+
+  if (!user) return <Redirect href="/" />
+
   return (
     <View>
       <SafeAreaView>
         <YStack>
           <Image
             source={{
-              uri: 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
+              uri:
+                user.coverImage ||
+                'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
               height: 140,
             }}
           />
@@ -23,20 +31,20 @@ export default function ProfileScreen() {
             >
               <Avatar.Image
                 source={{
-                  uri: 'https://randomuser.me/api/portraits/women/12.jpg',
+                  uri: user.profilePicture,
                   height: 140,
                 }}
               />
               <Avatar.Fallback />
             </Avatar>
             <YStack ai="center" gap="$2">
-              <H3>User name</H3>
-              <Paragraph color="$color05" mt="$-3">
-                @username
-              </Paragraph>
-              <Text>This is the user's bio</Text>
+              {user.fullName.length > 0 && <H3 mb="$-3">{user.fullName}</H3>}
+              <Paragraph color="$color05">@{user.username}</Paragraph>
+              <Text textAlign="center">
+                {user.bio.length > 0 ? user.bio : 'This user has not added a bio.'}
+              </Text>
             </YStack>
-            <Button size="$3" mt="$4" w="100%">
+            <Button size="$3" mt="$4" w="100%" backgroundColor="$blue8">
               Chat
             </Button>
           </YStack>
