200e37a3a61acde8b7d36597f2cdc9338ca8859d
Bhavishya <bhavishya1102@gmail.com>
2024-07-07 20:15:15 +0530

    refactor: Improve LoginForm component with form validation and submission
    The LoginForm component in LoginForm.tsx has been updated to include form validation and submission functionality using the react-hook-form library and zod schema validation. This refactor improves the user experience by validating user input and displaying error messages for invalid fields. Additionally, the form submission now sends a POST request to the '/auth/login' endpoint and handles the response accordingly.

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,32 +1,69 @@
-import { Button, Card, H2, ScrollView, View } from 'tamagui'
+import { Button, Card, Form, H2, Paragraph, ScrollView, View } from 'tamagui'
 import InputComponent from '../Form/Input'
 import { client } from '@/lib/axios'
 import useAuthStore from '@/state/authStore'
 import { router } from 'expo-router'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { AxiosError } from 'axios'
 
 export type TLoginFormProps = {
   handleSignUp: () => void
 }
 
+const LoginFormSchema = z.object({
+  email: z.string({ message: 'Email is required' }).email('Invalid email address'),
+  password: z.string({ message: 'Password is required' }),
+})
+
 export default function LoginForm({ handleSignUp }: TLoginFormProps) {
   const login = useAuthStore((state) => state.login)
 
-  const handleLogin = async () => {
+  const {
+    control,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(LoginFormSchema),
+    reValidateMode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const handleLogin = async (data: typeof LoginFormSchema._type) => {
     try {
-      const res = await client.post('/auth/login', {
-        email: 'test@test.com',
-        password: 'password',
-      })
+      const res = await client.post('/auth/login', data)
 
       if (res.data.error !== null) {
         // handle errors
+        console.error(res.data.error)
         return
       }
-      login(res.data.data.session)
+
+      const user = await client.get('/user', {
+        headers: {
+          Authorization: `Bearer ${res.data.data.session}`,
+        },
+      })
+
+      if (user.data.error !== null) {
+        // handle errors
+      }
+      login(res.data.data.session, user.data.data)
       router.push('/home')
-    } catch (err) {
-      router.push('/home')
+    } catch (err: any) {
       console.error(err)
+      if (err instanceof AxiosError && err.response) {
+        setError('root', {
+          message: err.response.data.error.message,
+        })
+
+        console.debug(err.response.data.error.message)
+      }
     }
   }
 
@@ -52,22 +89,64 @@ export default function LoginForm({ handleSignUp }: TLoginFormProps) {
           automaticallyAdjustKeyboardInsets
           showsVerticalScrollIndicator={false}
         >
-          <InputComponent
-            id="email_login"
-            label="Email"
-            inputProps={{ placeholder: 'johndoe@doemail.com' }}
-          />
-          <InputComponent
-            id="password_login"
-            label="Password"
-            inputProps={{ placeholder: 'Password123', secureTextEntry: true }}
-          />
-          <Button w="100%" mt="$2" onPress={handleLogin}>
-            Log in
-          </Button>
-          <Button unstyled w="100%" textAlign="center" mt="$4" onPress={handleSignUp}>
-            Don't have an account? Sign Up
-          </Button>
+          <Form onSubmit={handleSubmit(handleLogin)}>
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value, onBlur }, fieldState: { error } }) => (
+                <InputComponent
+                  id="email_login"
+                  label="Email"
+                  inputProps={{
+                    placeholder: 'johndoe@doemail.com',
+                    value,
+                    onBlur,
+                    onChangeText: onChange,
+                    autoCapitalize: 'none',
+                    keyboardType: 'email-address',
+                    // add red border if error
+                    borderColor: error ? 'red' : undefined,
+                  }}
+                  message={error?.message}
+                  messageProps={{
+                    color: 'red',
+                  }}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value, onBlur }, fieldState: { error } }) => (
+                <InputComponent
+                  id="password_login"
+                  label="Password"
+                  inputProps={{
+                    placeholder: 'Password123',
+                    secureTextEntry: true,
+                    value,
+                    onBlur,
+                    onChangeText: onChange,
+                    borderColor: error ? 'red' : undefined,
+                  }}
+                  message={error?.message}
+                  messageProps={{
+                    color: 'red',
+                  }}
+                />
+              )}
+            />
+            {errors.root && <Paragraph color="red">{errors.root.message}</Paragraph>}
+            <Form.Trigger asChild>
+              <Button w="100%" mt="$2">
+                Log in
+              </Button>
+            </Form.Trigger>
+            <Button unstyled w="100%" textAlign="center" mt="$4" onPress={handleSignUp}>
+              Don't have an account? Sign Up
+            </Button>
+          </Form>
         </ScrollView>
       </Card>
     </View>
