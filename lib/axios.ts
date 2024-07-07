8844114d08972edcec94969737552f953dcfcfb9
Bhavishya <bhavishya1102@gmail.com>
2024-07-07 20:14:55 +0530

    refactor: Update axios baseURL to use environment variable
    The axios baseURL in the axios.ts file has been updated to use the environment variable "EXPO_PUBLIC_API_BASE_URL" instead of the hard-coded value. This refactor improves the flexibility and maintainability of the code by allowing the baseURL to be easily changed without modifying the code itself.

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const client = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
