10e50a70e0199d6c6f84ea8d28214f32e506f094
Bhavishya <bhavishya1102@gmail.com>
2024-07-14 11:26:02 +0530

    refactor: Update communityStore.ts to include TCommunityDetail type
    The communityStore.ts file has been updated to include the TCommunityDetail type, which represents the detailed information of a community. This change enhances code clarity and maintainability by providing a specific type for community details, allowing for better organization and understanding of the data structure.

--- state/communityStore.ts
+++ state/communityStore.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand'
 import useAuthStore from './authStore'
 import { client } from '@/lib/axios'
+import type { TAxiosResponse } from '@/lib/types'
+import type { TTripOverview } from './tripStore'
 
 export type TCommunityMember = {
   id: number
@@ -43,17 +45,42 @@ export type TCommunityStore = {
   fetchCommunities: () => void
 }
 
+export type TCommunityDetail = {
+  id: number
+  name: string
+  description: string
+  createdBy: string
+  isPrivate: boolean
+  coverImage: string
+  profilePicture: string
+  trips: TTripOverview[]
+  members: (TCommunityMember & {
+    profile: {
+      fullName: string
+      profilePicture: string
+    }
+  })[]
+  memberCount: number
+}
+
 export const useCommunityStore = create<TCommunityStore>((set) => ({
   communities: [],
   fetchCommunities: async () => {
     try {
-      const response = await client.get('/community/overview', {
-        headers: {
-          Authorization: `Bearer ${useAuthStore.getState().token}`,
+      const response = await client.get<TAxiosResponse<TCommunityOverview[]>>(
+        '/community/overview',
+        {
+          headers: {
+            Authorization: `Bearer ${useAuthStore.getState().token}`,
+          },
         },
-      })
-      const data = await response.data.data
-      console.debug('Communities:', data[1].members)
+      )
+
+      if (response.data.error) {
+        throw new Error(response.data.error.message)
+      }
+
+      const data = response.data.data
       set({ communities: data })
     } catch (e) {
       console.error(e)
