bcd2c9f92ea8f2ee9644ec1f38a209ba9eb45e78
Bhavishya <bhavishya1102@gmail.com>
2024-07-22 11:26:21 +0530

    ```text refactor: Update CommunitiesScreen to use FlatList for rendering community cards
    The CommunitiesScreen component in communities.tsx has been updated to use FlatList instead of ScrollView for rendering the community cards. This change improves the performance and memory efficiency by rendering only the visible community cards, rather than rendering all cards at once. It also provides a smoother scrolling experience for users.

--- app/communities.tsx
+++ app/communities.tsx
@@ -1,36 +1,31 @@
 import { CommunityCard } from '@/components/CommunityCard'
 import { useCommunityStore } from '@/state/communityStore'
 import { router } from 'expo-router'
-import { SafeAreaView } from 'react-native'
-import { ScrollView, YStack } from 'tamagui'
+import { FlatList, SafeAreaView } from 'react-native'
+import { YStack } from 'tamagui'
 
 export default function CommunitiesScreen() {
   const communities = useCommunityStore((state) => state.communities)
   return (
     <SafeAreaView style={{ height: '100%' }}>
-      <ScrollView
-        contentContainerStyle={{
-          flexGrow: 1,
-          justifyContent: 'space-between',
-          overflow: 'hidden',
-        }}
-      >
-        <YStack gap="$2" mx="$2">
-          {communities.length > 0 &&
-            communities.map((c) => (
-              <CommunityCard
-                key={c.id}
-                title={c.name}
-                description={c.description}
-                memberCount={c.memberCount}
-                members={c.members}
-                onPress={() => {
-                  router.push(`community/${c.id}`)
-                }}
-              />
-            ))}
-        </YStack>
-      </ScrollView>
+      <YStack gap="$2" mx="$2">
+        <FlatList
+          data={communities}
+          style={{ width: '100%', height: '100%' }}
+          renderItem={({ item: c }) => (
+            <CommunityCard
+              key={c.id}
+              title={c.name}
+              description={c.description}
+              memberCount={c.memberCount}
+              members={c.members}
+              onPress={() => {
+                router.push(`community/${c.id}`)
+              }}
+            />
+          )}
+        />
+      </YStack>
     </SafeAreaView>
   )
 }
