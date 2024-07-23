1bca3d2552866b3e9b6e74ce5c0a74c8936eae91
Bhavishya <bhavishya1102@gmail.com>
2024-07-23 11:03:25 +0530

    refactor: Update DatePickerInput component
    The DatePickerInput component in components/Form/DatePicker.tsx has been updated to support an inline variant, allowing for a different layout and styling. This change improves the flexibility of the component and enhances the user experience by providing more options for displaying date pickers.

--- components/Form/DatePicker.tsx
+++ components/Form/DatePicker.tsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import { Keyboard, Platform, TouchableWithoutFeedback } from 'react-native'
+import { Keyboard, Platform } from 'react-native'
 import { type Control, type FieldValues, type Path, useController } from 'react-hook-form'
 import DateTimePicker, {
   type DateTimePickerEvent,
   type DatePickerOptions,
 } from '@react-native-community/datetimepicker'
-import { Input, View, Text } from 'tamagui'
+import { Input, View, Text, Sheet, Button, YStack, XStack } from 'tamagui'
 
 type TDatePickerInputProps<T extends FieldValues> = {
   name: Path<T>
   control: Control<T>
   label: string
   datePickerProps?: Omit<DatePickerOptions, 'value' | 'mode' | 'onChange'>
+  variant?: 'default' | 'inline'
+  required?: boolean
 }
 
 const DatePickerInput = <T extends FieldValues>({
@@ -19,8 +21,10 @@ const DatePickerInput = <T extends FieldValues>({
   control,
   label,
   datePickerProps,
+  variant = 'default',
+  required = false,
 }: TDatePickerInputProps<T>) => {
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
   const {
     field: { value, onChange, onBlur },
     fieldState: { error },
@@ -29,58 +33,86 @@ const DatePickerInput = <T extends FieldValues>({
     control,
   })
 
-  const showDatePicker = () => {
-    setDatePickerVisibility(true)
-  }
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false)
-  }
-
   const handleConfirm = (event: DateTimePickerEvent, selectedDate?: Date) => {
-    // hideDatePicker()
     if (selectedDate) {
       onChange(selectedDate.toISOString())
     }
   }
 
-  const handleBlur = () => {
+  const handleClose = () => {
+    setIsOpen(false)
     onBlur()
-    hideDatePicker()
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View>
-        <Text>{label}</Text>
-        <Input
-          value={value ? new Date(value).toLocaleString() : ''}
-          onPressIn={isDatePickerVisible ? hideDatePicker : showDatePicker}
-          placeholder="Select a date"
-          editable={false}
-          onBlur={handleBlur}
-        />
-        {error && <Text color="red">{error.message}</Text>}
+    <View>
+      <YStack py="$1">
+        {variant === 'default' ? (
+          <>
+            <Text>
+              {label}
+              {required ? '*' : ''}
+            </Text>
+            <Input
+              value={value ? new Date(value).toLocaleString() : ''}
+              onPressIn={() => {
+                Keyboard.dismiss()
+                setIsOpen(true)
+              }}
+              placeholder="Select a date"
+              editable={false}
+            />
+          </>
+        ) : (
+          <XStack gap="$4" jc="space-between" ai="baseline">
+            <Text color="$color05" fontSize="$4">
+              {label}
+              {required ? '*' : ''}
+            </Text>
+            <Input
+              value={value ? new Date(value).toLocaleString() : ''}
+              onPressIn={() => {
+                Keyboard.dismiss()
+                setIsOpen(true)
+              }}
+              placeholder="Select a date"
+              editable={false}
+              unstyled
+              color="$color"
+              fontSize="$6"
+              display="flex"
+              flexShrink={1}
+              overflow="scroll"
+            />
+          </XStack>
+        )}
+        {error && <Text style={{ color: 'red' }}>{error.message}</Text>}
+      </YStack>
 
-        {isDatePickerVisible && (
+      <Sheet
+        modal
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        snapPoints={[40]}
+        position={0}
+        dismissOnSnapToBottom
+      >
+        <Sheet.Overlay />
+        <Sheet.Frame alignItems="center" justifyContent="center">
+          <Sheet.Handle />
           <DateTimePicker
             value={value ? new Date(value) : new Date()}
             mode="datetime"
             display={Platform.OS === 'ios' ? 'spinner' : 'default'}
             onChange={handleConfirm}
             {...datePickerProps}
-            // For iOS, we need to add a button to close the picker
-            {...(Platform.OS === 'ios' && {
-              onCancel: hideDatePicker,
-            })}
           />
-        )}
-
-        {/* {Platform.OS === 'ios' && isDatePickerVisible && (
-          <Button onPress={hideDatePicker}>Confirm</Button>
-        )} */}
-      </View>
-    </TouchableWithoutFeedback>
+          <Button onPress={handleClose} disabled={!value} marginTop="$4">
+            Confirm
+          </Button>
+        </Sheet.Frame>
+      </Sheet>
+    </View>
   )
 }
 
