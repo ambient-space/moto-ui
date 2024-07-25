2f230f4331a93ca8bf8ff05baa895b197cc643fe
Bhavishya <bhavishya1102@gmail.com>
2024-07-25 18:00:20 +0530

    refactor: Update HomeScreen to use FlatList for displaying trips
    The code changes in `home.tsx` modify the `HomeScreen` component to use `FlatList` instead of `ScrollView` for displaying trips. This change improves the performance and scrolling experience by rendering only the visible items and recycling the off-screen ones. By utilizing `FlatList`, the HomeScreen provides a more efficient and responsive display of trips.

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -3,8 +3,9 @@ import { useCommunityStore } from '@/state/communityStore'
 import { useTripStore } from '@/state/tripStore'
 import { router } from 'expo-router'
 import { Button, H1, H3, Paragraph, ScrollView, Text, XStack, YStack } from 'tamagui'
-import { TripCard } from '@/components/TripCard'
 import { useEffect } from 'react'
+import { TripCard } from '@/components/TripCard'
+import { FlatList } from 'react-native'
 
 export default function HomeScreen() {
   const { communities, fetchCommunities } = useCommunityStore((state) => state)
@@ -17,8 +18,8 @@ export default function HomeScreen() {
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <YStack f={1} gap="$4" pt="$5" pb="$5">
-        <YStack>
+      <YStack f={1} gap="$4" pb="$5">
+        <YStack backgroundColor="$background" py="$6">
           <H1 textAlign="left" width="100%" px="$4" fontWeight="$2">
             Welcome to{' '}
             <Text fontWeight="$16" color="$green10Light">
@@ -30,7 +31,7 @@ export default function HomeScreen() {
           </Paragraph>
         </YStack>
         <XStack gap="$4" ai="center" jc="space-between" px="$4">
-          <H3 textAlign="left">Trips near you</H3>
+          <H3 textAlign="left">Upcoming Trips</H3>
           <Button
             unstyled
             color="$blue10"
@@ -41,7 +42,7 @@ export default function HomeScreen() {
             View more
           </Button>
         </XStack>
-        {trips.length > 0 ? (
+        {/* {trips.length > 0 ? (
           <ScrollView
             contentContainerStyle={{
               backgroundColor: '$colorTransparent',
@@ -51,29 +52,61 @@ export default function HomeScreen() {
             px="$4"
             showsHorizontalScrollIndicator={false}
           >
-            <XStack gap="$4" pr="$4">
-              {trips.length > 0 &&
-                trips.map((t) => (
-                  <TripCard
-                    key={t.id}
-                    title={t.name}
-                    startDate={new Date(t.startDate).toLocaleString()}
-                    startLocation={t.startLocation}
-                    description={t.description}
-                    participants={t.participants}
-                    participantCount={t.participantCount}
-                    onPress={() => {
-                      router.push(`trip/${t.id}`)
-                    }}
-                  />
-                ))}
+            <XStack gap="$4">
+              {trips.map((t) => (
+                <TripCard
+                  key={t.id}
+                  title={t.name}
+                  startDate={new Date(t.startDate).toLocaleString()}
+                  startLocation={t.startLocation}
+                  description={t.description}
+                  participants={t.participants}
+                  participantCount={t.participantCount}
+                  onPress={() => {
+                    router.push(`trip/${t.id}`)
+                  }}
+                />
+              ))}
             </XStack>
           </ScrollView>
         ) : (
           <Text px="$4">No trips found</Text>
+        )} */}
+
+        {trips.length > 0 ? (
+          <FlatList
+            data={trips}
+            renderItem={({ item }) => (
+              <TripCard
+                key={item.id}
+                title={item.name}
+                startDate={new Date(item.startDate).toLocaleString()}
+                startLocation={item.startLocation}
+                description={item.description}
+                participants={item.participants}
+                participantCount={item.participantCount}
+                onPress={() => {
+                  router.push(`trip/${item.id}`)
+                }}
+                w={320}
+              />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              backgroundColor: '$colorTransparent',
+              paddingLeft: 16,
+              paddingRight: 16,
+              gap: 16,
+            }}
+            style={{ flexGrow: 0 }}
+          />
+        ) : (
+          <Text px="$4">No trips found</Text>
         )}
         <XStack gap="$4" ai="center" jc="space-between" px="$4">
-          <H3 textAlign="left">Communities for you</H3>
+          <H3 textAlign="left">Featured Communities</H3>
           <Button
             unstyled
             color="$blue10"
@@ -85,31 +118,32 @@ export default function HomeScreen() {
           </Button>
         </XStack>
         {communities.length > 0 ? (
-          <ScrollView
+          <FlatList
+            data={communities}
+            renderItem={({ item }) => (
+              <CommunityCard
+                memberCount={item.memberCount}
+                members={item.members}
+                key={item.id}
+                title={item.name}
+                description={item.description}
+                onPress={() => {
+                  router.push(`community/${item.id}`)
+                }}
+                w={320}
+              />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+            showsHorizontalScrollIndicator={false}
             contentContainerStyle={{
               backgroundColor: '$colorTransparent',
+              paddingLeft: 16,
+              paddingRight: 16,
+              gap: 16,
             }}
-            horizontal
             style={{ flexGrow: 0 }}
-            px="$4"
-            showsHorizontalScrollIndicator={false}
-          >
-            <XStack gap="$4" pr="$4">
-              {communities.map((c) => (
-                <CommunityCard
-                  key={c.id}
-                  title={c.name}
-                  description={c.description}
-                  memberCount={c.memberCount}
-                  members={c.members}
-                  w="$18"
-                  onPress={() => {
-                    router.push(`community/${c.id}`)
-                  }}
-                />
-              ))}
-            </XStack>
-          </ScrollView>
+          />
         ) : (
           <Text px="$4">No communities found</Text>
         )}
