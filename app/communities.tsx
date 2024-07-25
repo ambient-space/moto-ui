81f25b07e5095c7c54bf0d7e868dc5e90e030db4
Bhavishya <bhavishya1102@gmail.com>
2024-07-25 17:58:22 +0530

    refactor: Update CommunitiesScreen to improve UI and layout
    The code changes in `communities.tsx` modify the `CommunitiesScreen` component to improve the UI and layout. The `YStack` component no longer has a horizontal margin, and the `FlatList` now has a contentContainerStyle with a gap of 8 and padding on the left and right sides. Additionally, the `CommunityCard` component now has a width of 100%. These changes enhance the visual presentation and user experience of the communities screen.

--- app/communities.tsx
+++ app/communities.tsx
@@ -8,13 +8,15 @@ export default function CommunitiesScreen() {
   const communities = useCommunityStore((state) => state.communities)
   return (
     <SafeAreaView style={{ height: '100%' }}>
-      <YStack gap="$2" mx="$2">
+      <YStack gap="$2">
         <FlatList
           data={communities}
           style={{ width: '100%', height: '100%' }}
+          contentContainerStyle={{ gap: 8, paddingLeft: 16, paddingRight: 16 }}
           renderItem={({ item: c }) => (
             <CommunityCard
               key={c.id}
+              w="100%"
               title={c.name}
               description={c.description}
               memberCount={c.memberCount}
