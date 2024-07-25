d82ffb2216b2e55896ce5ae99d7696c39f352751
Bhavishya <bhavishya1102@gmail.com>
2024-07-25 17:54:22 +0530

    refactor: Update MemberCard to display current user indicator
    The code changes in `MemberCard.tsx` modify the `MemberCard` component to display an indicator for the current user. The `isCurrentUser` prop is added to the component, and if it is true, the component displays "(You)" next to the user's name. This refactor improves the user experience by providing a visual cue for the current user and distinguishing them from other members.

--- components/MemberCard.tsx
+++ components/MemberCard.tsx
@@ -9,6 +9,7 @@ type TMemberCardProps = {
       fullName: string
     }
     role: string
+    isCurrentUser?: boolean
   }
   onPress?: () => void
 }
@@ -21,20 +22,23 @@ const MemberCard = ({ member, onPress }: TMemberCardProps) => {
 
   return (
     <XStack gap="$2" ai="center" onPress={handleOnPress}>
-      <Avatar circular size="$4" zIndex={2} borderColor="white" borderWidth="$0.5">
+      <Avatar circular size="$4">
         <Avatar.Image
           source={{
             uri:
               member.profile.profilePicture ||
-              'https://png.pngtree.com/png-vector/20210604/ourmid/pngtree-gray-avatar-placeholder-png-image_3416697.jpg',
-            height: 140,
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdFe2l5yrU1Bd9lao2sMQTAFu0XVbDxk0GVQ&s',
           }}
         />
         <Avatar.Fallback backgroundColor="$blue10" />
       </Avatar>
-      <YStack>
-        <Text fontSize={16}>{member.profile.fullName || 'New User'}</Text>
-        <Text fontSize={12}>{member.role}</Text>
+      <YStack gap="$2">
+        <Text fontSize={16} fontWeight="600" textTransform="capitalize">
+          {member.profile.fullName || 'New User'} {member.isCurrentUser && '(You)'}
+        </Text>
+        <Text fontSize={14} theme="alt1" textTransform="capitalize">
+          {member.role}
+        </Text>
       </YStack>
     </XStack>
   )
