f9da8f0389271ff9168e4f54865c78dd0d80652f
Bhavishya <bhavishya1102@gmail.com>
2024-08-01 20:58:00 +0530

    refactor: refresh functionality and use react-query hooks on home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,23 +1,37 @@
 import { CommunityCard } from '@/components/CommunityCard'
-import { useCommunityStore } from '@/state/communityStore'
-import { useTripStore } from '@/state/tripStore'
+import { useTripHomeHook } from '@/state/tripStore'
 import { router } from 'expo-router'
 import { Button, H1, H3, Paragraph, ScrollView, Text, XStack, YStack } from 'tamagui'
-import { useEffect } from 'react'
 import { TripCard } from '@/components/TripCard'
-import { FlatList } from 'react-native'
+import { FlatList, RefreshControl } from 'react-native'
+import { CardSkeleton } from '@/components/CardSkeleton'
+import { useCommunityHomeHook } from '@/state/communityStore'
 
 export default function HomeScreen() {
-  const { communities, fetchCommunities } = useCommunityStore((state) => state)
-  const { trips, fetchTrips } = useTripStore((state) => state)
-
-  useEffect(() => {
-    fetchTrips()
-    fetchCommunities()
-  }, [])
+  const {
+    data: trips,
+    isLoading: areTripsLoading,
+    refetch: refetchTrips,
+  } = useTripHomeHook()
+  const {
+    data: communities,
+    isLoading: areCommunitiesLoading,
+    refetch: refetchCommunities,
+  } = useCommunityHomeHook()
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl
+          refreshing={areTripsLoading || areCommunitiesLoading}
+          onRefresh={() => {
+            refetchTrips()
+            refetchCommunities()
+          }}
+        />
+      }
+    >
       <YStack f={1} gap="$4" pb="$5">
         <YStack backgroundColor="$background" py="$6">
           <H1 textAlign="left" width="100%" px="$4" fontWeight="$2">
@@ -42,68 +56,47 @@ export default function HomeScreen() {
             View more
           </Button>
         </XStack>
-        {/* {trips.length > 0 ? (
-          <ScrollView
-            contentContainerStyle={{
-              backgroundColor: '$colorTransparent',
-            }}
-            horizontal
-            style={{ flexGrow: 0 }}
-            px="$4"
-            showsHorizontalScrollIndicator={false}
-          >
-            <XStack gap="$4">
-              {trips.map((t) => (
-                <TripCard
-                  key={t.id}
-                  title={t.name}
-                  startDate={new Date(t.startDate).toLocaleString()}
-                  startLocation={t.startLocation}
-                  description={t.description}
-                  participants={t.participants}
-                  participantCount={t.participantCount}
-                  onPress={() => {
-                    router.push(`trip/${t.id}`)
-                  }}
-                />
-              ))}
-            </XStack>
-          </ScrollView>
-        ) : (
-          <Text px="$4">No trips found</Text>
-        )} */}
 
-        {trips.length > 0 ? (
-          <FlatList
-            data={trips}
-            renderItem={({ item }) => (
-              <TripCard
-                key={item.id}
-                title={item.name}
-                startDate={new Date(item.startDate).toLocaleString()}
-                startLocation={item.startLocation}
-                description={item.description}
-                participants={item.participants}
-                participantCount={item.participantCount}
-                onPress={() => {
-                  router.push(`trip/${item.id}`)
-                }}
-                w={320}
-              />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-              backgroundColor: '$colorTransparent',
-              paddingLeft: 16,
-              paddingRight: 16,
-              gap: 16,
-            }}
-            style={{ flexGrow: 0 }}
-          />
+        {!areTripsLoading ? (
+          trips.length > 0 ? (
+            <FlatList
+              data={trips}
+              renderItem={({ item }) => (
+                <TripCard
+                  isParticipant={item.isParticipant}
+                  key={item.id}
+                  title={item.name}
+                  startDate={new Date(item.startDate).toLocaleString()}
+                  startLocation={item.startLocation}
+                  description={item.description}
+                  participants={item.participants}
+                  participantCount={item.participantCount}
+                  onPress={() => {
+                    router.push(`trip/${item.id}`)
+                  }}
+                  w={320}
+                />
+              )}
+              maxToRenderPerBatch={2}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                backgroundColor: '$colorTransparent',
+                paddingLeft: 16,
+                paddingRight: 16,
+                gap: 16,
+              }}
+              style={{ flexGrow: 0 }}
+            />
+          ) : (
+            <Text px="$4">No trips found</Text>
+          )
         ) : (
-          <Text px="$4">No trips found</Text>
+          <XStack gap="$4" px="$4">
+            <CardSkeleton />
+            <CardSkeleton />
+          </XStack>
         )}
         <XStack gap="$4" ai="center" jc="space-between" px="$4">
           <H3 textAlign="left">Featured Communities</H3>
@@ -117,35 +110,43 @@ export default function HomeScreen() {
             View more
           </Button>
         </XStack>
-        {communities.length > 0 ? (
-          <FlatList
-            data={communities}
-            renderItem={({ item }) => (
-              <CommunityCard
-                memberCount={item.memberCount}
-                members={item.members}
-                key={item.id}
-                title={item.name}
-                description={item.description}
-                onPress={() => {
-                  router.push(`community/${item.id}`)
-                }}
-                w={320}
-              />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{
-              backgroundColor: '$colorTransparent',
-              paddingLeft: 16,
-              paddingRight: 16,
-              gap: 16,
-            }}
-            style={{ flexGrow: 0 }}
-          />
+        {!areCommunitiesLoading ? (
+          communities.length > 0 ? (
+            <FlatList
+              data={communities}
+              renderItem={({ item }) => (
+                <CommunityCard
+                  memberCount={item.memberCount}
+                  members={item.members}
+                  key={item.id}
+                  title={item.name}
+                  description={item.description}
+                  isMember={item.isMember}
+                  onPress={() => {
+                    router.push(`community/${item.id}`)
+                  }}
+                  w={320}
+                />
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{
+                backgroundColor: '$colorTransparent',
+                paddingLeft: 16,
+                paddingRight: 16,
+                gap: 16,
+              }}
+              style={{ flexGrow: 0 }}
+            />
+          ) : (
+            <Text px="$4">No communities found</Text>
+          )
         ) : (
-          <Text px="$4">No communities found</Text>
+          <XStack gap="$4" px="$4">
+            <CardSkeleton />
+            <CardSkeleton />
+          </XStack>
         )}
       </YStack>
     </ScrollView>
