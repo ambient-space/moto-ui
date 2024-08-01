cdb09188ee1c087b7cb5d5a3218bcb21a79f9ebc
Bhavishya <bhavishya1102@gmail.com>
2024-08-01 20:57:19 +0530

    ```text refactor: Update Settings page UI with lazy loading for AddVehicle component
    The code changes in `settings.tsx` refactor the UI of the Settings page by implementing lazy loading for the AddVehicle component. This enhancement improves the page load performance by loading the AddVehicle component only when it is needed, reducing the initial load time for the Settings page.

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,23 +1,31 @@
+import AddVehicle from '@/components/AddVehicle'
+import CustomSettingsButton from '@/components/CustomSettingsButton'
 import useAuthStore from '@/state/authStore'
-import { router } from 'expo-router'
-import { Button, Text, View } from 'tamagui'
+import { LogOut } from '@tamagui/lucide-icons'
+import { ScrollView, YStack } from 'tamagui'
 
 export default function Settings() {
   const logout = useAuthStore((state) => state.logout)
   return (
-    <View flex={1} alignItems="center" justifyContent="center">
-      <Text fontSize={20} color="$blue10">
-        Settings
-      </Text>
-      <Button
-        onPress={() => {
-          logout()
-          router.replace('/')
-        }}
-        color="$red10"
-      >
-        Logout
-      </Button>
-    </View>
+    <ScrollView
+      automaticallyAdjustKeyboardInsets
+      showsVerticalScrollIndicator={false}
+      keyboardDismissMode="on-drag"
+    >
+      <YStack gap="$4" p="$4">
+        <YStack gap="$2" backgroundColor="$background" p="$4" borderRadius="$4">
+          <AddVehicle />
+        </YStack>
+
+        <CustomSettingsButton
+          onPress={logout}
+          backgroundColor="$red10"
+          color="$white1"
+          icon={LogOut}
+        >
+          Logout
+        </CustomSettingsButton>
+      </YStack>
+    </ScrollView>
   )
 }
