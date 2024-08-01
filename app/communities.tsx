4d99c5090975545e4d82db30c5fccbf8e9e7328c
Bhavishya <bhavishya1102@gmail.com>
2024-08-01 20:55:35 +0530

    feat: use react query hooks and add load more and refresh functionality for communities

--- app/communities.tsx
+++ app/communities.tsx
@@ -1,11 +1,31 @@
 import { CommunityCard } from '@/components/CommunityCard'
-import { useCommunityStore } from '@/state/communityStore'
+import { useCommunityPageHook } from '@/state/communityStore'
 import { router } from 'expo-router'
-import { FlatList, SafeAreaView } from 'react-native'
-import { YStack } from 'tamagui'
+import { FlatList, RefreshControl, SafeAreaView } from 'react-native'
+import { Spinner, YStack } from 'tamagui'
 
 export default function CommunitiesScreen() {
-  const communities = useCommunityStore((state) => state.communities)
+  const {
+    data: communities,
+    isLoading,
+    refetch,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useCommunityPageHook()
+
+  const loadMore = () => {
+    if (isFetchingNextPage) return null
+    if (hasNextPage && !isFetchingNextPage) {
+      fetchNextPage()
+    }
+  }
+
+  const renderFooter = () => {
+    if (!isFetchingNextPage) return null
+    return <Spinner style={{ marginVertical: 20 }} size="large" />
+  }
+
   return (
     <SafeAreaView style={{ height: '100%' }}>
       <YStack gap="$2">
@@ -13,6 +33,18 @@ export default function CommunitiesScreen() {
           data={communities}
           style={{ width: '100%', height: '100%' }}
           contentContainerStyle={{ gap: 8, paddingLeft: 16, paddingRight: 16 }}
+          onEndReached={loadMore}
+          onEndReachedThreshold={0.2}
+          refreshing={isLoading}
+          refreshControl={
+            <RefreshControl
+              refreshing={isLoading}
+              onRefresh={() => {
+                refetch()
+              }}
+            />
+          }
+          ListFooterComponent={renderFooter}
           renderItem={({ item: c }) => (
             <CommunityCard
               key={c.id}
@@ -21,6 +53,7 @@ export default function CommunitiesScreen() {
               description={c.description}
               memberCount={c.memberCount}
               members={c.members}
+              isMember={c.isMember}
               onPress={() => {
                 router.push(`community/${c.id}`)
               }}
