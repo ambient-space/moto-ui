f60e9e75e7990f06b304ba912b30a3248a65535d
Bhavishya <bhavishya1102@gmail.com>
2024-08-06 16:02:13 +0530

    update login check

--- app/index.tsx
+++ app/index.tsx
@@ -8,9 +8,12 @@ import { AnimatePresence, ScrollView } from 'tamagui'
 
 export default function Auth() {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+  const user = useAuthStore((state) => state.user)
   const [showLogin, setShowLogin] = useState(true)
 
-  if (isAuthenticated) return <Redirect href="/home" />
+  if (isAuthenticated && user && user.profile) {
+    return <Redirect href="/home" />
+  }
   return (
     <SafeAreaView>
       <ScrollView
